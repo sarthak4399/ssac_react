e405c264760c7e2edf46d68ec683414d1ae3fa33
sarthak4399 <sarthakkhandare21@gmail.com>
2023-05-28 12:10:09 +0530

    car component added

--- src/components/Exams.jsx
+++ src/components/Exams.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 import "./Exams.css";
 import Card from "./Card";
+const countries = [
+  {
+    name: "Country 1",
 
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },  {
+    name: "Country 1",
+
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.kdbAe7rgxjrkP9P2irwn3QHaE_%26pid%3DApi&f=1&ipt=f22b4c69a8c14698a76bfdad33c70df92705b0bf0d41effb8513eeaec79f2d80&ipo=images",
+    Exams: " Tofel ",
+    region: "Region 1"
+  },
+];
 export const Exams = () => {
   return (
     <div className="exams-section">
@@ -17,11 +67,11 @@ export const Exams = () => {
       </div>
       <div className="e-bottom">
         
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className="c-country-list">
+      {countries.map((country, index) => (
+        <Card key={index} country={country} />
+      ))}
+    </div>
       </div>
     </div>
   );
